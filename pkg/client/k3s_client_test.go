b2fd33274e5ff5f020e5d82f6367df800403a07a
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2024-07-07 21:18:43 -0300

    Fixes Tests

--- pkg/client/k3s_client_test.go
+++ pkg/client/k3s_client_test.go
@@ -15,6 +15,7 @@ func TestK3sClient_ConfigureMasterNode(t *testing.T) {
 	nodeArgs := []string{
 		"--disable traefik",
 		"--node-label node_type=master",
+		"--snapshotter native",
 	}
 
 	var nodeConfig = resources.K3sMasterNodeConfig{
@@ -40,35 +41,37 @@ func TestK3sClient_ConfigureMasterNode(t *testing.T) {
 
 func TestK3sClient_ConfigureWorkerNode(t *testing.T) {
 	host := "localhost"
-	port := "2222"
+	port := "3333"
 	user := "sshuser"
 	password := "password"
 
 	nodeArgs := []string{
 		"--node-label node_type=worker",
+		"--snapshotter native",
+	}
+
+	var masterNodeConfig = resources.K3sMasterNodeConfig{
+		Host:    host,
+		Token:   "token",
+		Version: "v1.30.2+k3s1",
+		ConnectionConfig: ssh_handler.SshConfig{
+			Host:     host,
+			Port:     port,
+			User:     user,
+			Password: password,
+		},
 	}
 
 	var nodeConfig = resources.K3sWorkerNodeConfig{
-		Server: "master_node",
-		K3sMasterNodeConfig: resources.K3sMasterNodeConfig{
-			Host:    host,
-			Token:   "token",
-			Version: "v1.30.2+k3s1",
-			ConnectionConfig: ssh_handler.SshConfig{
-				Host:     host,
-				Port:     port,
-				User:     user,
-				Password: password,
-			},
-		},
+		Server:              "master_node",
+		K3sMasterNodeConfig: masterNodeConfig,
 	}
 
 	c := NewK3sClient()
 
 	err := c.ConfigureWorkerNode(nodeConfig, nodeArgs)
 	if err != nil {
-		//t.Errorf("Error configuring worker node: %v", err)
-		// Expecting error because the server is not running
+		t.Errorf("Error configuring worker node: %v", err)
 		return
 	}
 }
